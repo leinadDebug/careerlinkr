fad4255a002555262bbde489c936241ece6b4f32
leinadDebug <akujuaobidaniel@gmail.com>
2024-05-04 21:56:42 +0100

    - Received search parameters as props to determine initial filter values for job filtering. - Constructed filter values based on the received search parameters, including search query, job type, location, and remote work preference. - Rendered the JobFilterSidebar component to provide users with filtering options for refining job search criteria. - Rendered the JobResults component to display job listings based on the applied filter values, providing users with relevant search results.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,37 @@
+import H1 from "@/components/ui/h1";
 import JobFilterSidebar from "@/components/ui/jobFilterSidebar";
-import Joblisting from "@/components/ui/joblisting";
-import prisma from "@/lib/prisma";
+import JobResults from "@/components/ui/jobResult";
+import { JobFilterValues } from "@/lib/validation";
 
-export default async function Home() {
-  const jobs = await prisma.job.findMany({
-    where: { approved: true },
-    orderBy: { createdAt: "desc" },
-  });
+interface PageProps {
+  searchParams: {
+    search?: string;
+    type?: string;
+    location?: string;
+    remote?: string;
+  };
+}
+
+export default async function Home({
+  searchParams: {search, type, location, remote},
+}: PageProps) {
+  const filterValues: JobFilterValues = {
+    search,
+    type,
+    location,
+    remote: remote === "true",
+  };
   return (
     <main className="m-auto my-10 max-w-5xl space-y-10 px-3">
       <div className="space-y-5 text-center">
-        <h1 className="text-center text-2xl font-extrabold tracking-tight lg:text-4xl">
-          Developer Jobs
-        </h1>
+        <H1>Developer Jobs</H1>
         <p className="text-muted-foreground">
           Find the perfect developer job for you.
         </p>
       </div>
-      <section className="flex flex-col md:flex-row gap-4">
-        <JobFilterSidebar/>
-        <div className="space-y-4 grow">
-          {jobs.map((job) => (
-            <Joblisting job={job} key={job.id} />
-          ))}
-        </div>
+      <section className="flex flex-col gap-4 md:flex-row">
+        <JobFilterSidebar />
+        <JobResults filterValues={filterValues} />
       </section>
     </main>
   );
