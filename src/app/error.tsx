e1c4aeee7da29a9c736b0d15a8df1af8a7f23e19
leinadDebug <akujuaobidaniel@gmail.com>
2024-05-04 21:52:02 +0100

    feat: Add Error component
    Description:
    This commit adds the Error component, which is designed to handle unexpected errors in the application. When an unexpected error occurs, this component will be rendered to inform the user of the issue.
    
    Changes:
    - Created the Error component to encapsulate error handling logic and UI.
    - Utilized client-side rendering for improved performance and responsiveness.
    - Integrated the H1 component to display a clear and prominent error title.
    - Included an error image using the Next.js Image component to provide a visual representation of the error.
    - Applied styling to the components to ensure a consistent and visually appealing user interface.
    
    Purpose:
    The Error component enhances the user experience by gracefully handling unexpected errors and providing clear feedback to the user. By implementing this component, we improve the robustness and usability of the application.

---
+++ src/app/error.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import H1 from "@/components/ui/h1";
+import Img from "../../public/inactiveBalloonMonsterFront.4eca22acdc791643a1c1.webp";
+import Image from "next/image";
+
+export default function Error() {
+  return (
+    <main className="m-auto my-7 flex max-w-5xl flex-col items-center space-y-5 p-3 text-center">
+      <div>
+        <H1>Error</H1>
+        <p className="text-muted-foreground">An unexpected error occurred</p>
+      </div>
+      <Image
+        alt="error"
+        src={Img}
+        className="rounded-full  bg-[#fffdfdbc] p-3"
+      ></Image>
+    </main>
+  );
+}
